fda184c614601908cdca85ca980b4e3f09d7190e
Atlas Yiğit Aydın <atlasyigitaydin@gmail.com>
2026-09-03 18:16:56 +0300

    v2.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/content'],
   css: ['~/assets/css/main.css'],
+  nitro: {
+    prerender: {
+      routes: ['/'],
+      crawlLinks: true,
+    },
+  },
 })
